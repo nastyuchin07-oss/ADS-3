707f2fb9dc9b7eb200a76dd9f6fbd9b5cc0cdf99
nastyuchin07-oss <nastyuchin07@gmail.com>
2026-03-15 16:44:51 +0300

    Сергиенко А. В. 25-ИСТ-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,54 @@
 // Copyright 2025 NNTU-CS
-#include <cstdint>
 #include "alg.h"
+#include <iostream>
+#include <cstdint>
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+    uint64_t maxVal = num;
+
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            num = 3 * num + 1;
+        }
+
+        if (num > maxVal) {
+            maxVal = num;
+        }
+    }
+
+    return maxVal;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned int length = 1;
+
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            num = 3 * num + 1;
+        }
+
+        length++;
+    }
+
+    return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound, uint64_t rbound) {
+    unsigned int number = 0;
+    *maxlen = 0;
+
+    for (uint64_t i = lbound; i <= rbound; i++) {
+        unsigned int len = collatzLen(i);
+
+        if (len > *maxlen) {
+            *maxlen = len;
+            number = i;
+        }
+    }
+
+    return number;
 }
